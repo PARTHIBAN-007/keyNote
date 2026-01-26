a9822445f19f7f0a4490068cc00a97e9da1ca3e8
PARTHIBAN-007 <parthiban.ktech@gmail.com>
2026-01-26 20:41:47 +0530

    Azure Transcription

--- agent_api/src/routers/transcribe.py
+++ agent_api/src/routers/transcribe.py
@@ -8,15 +8,8 @@ router = APIRouter(tags = ["Transcription"])
 @router.post("/transcribe")
 async def transcribe(background_tasks: BackgroundTasks, file: UploadFile = File(...)):
     try:
-        # 1. Get Service Instance
         transcription_service = get_transcription_service()
-
-        # 2. Upload File
-        # We pass the file stream directly to the service
         transcription_service.upload_file(file.file, file.filename)
-        
-        # 3. Add to Background Tasks
-        # We pass the class method so it runs asynchronously after the response
         background_tasks.add_task(transcription_service.run_transcription, file.filename)
         
         return {
